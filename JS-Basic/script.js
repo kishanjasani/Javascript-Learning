b7e22c25aae5c2a8e3597b12cc5aaaf9ba505ffd
Kishan Jasani <kishanjasani007@yahoo.in>
2019-05-23 14:27:49 +0530

    Add datatype and variables example

--- JS-Basic/script.js
+++ JS-Basic/script.js
@@ -1,1 +1,12 @@
 console.log( 'Hello World!' );
+
+var name = 'Kishan';
+
+var lastName = 'Jasani'
+console.log( 'Hello ' + name + ' ' + lastName );
+
+var age = '23';
+console.log( age );
+
+var fullAge = true;
+console.log( fullAge );
