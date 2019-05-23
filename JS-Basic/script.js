37bef31b3c1ad4c5af3b63aa56425d62c2643ecb
Kishan Jasani <kishanjasani007@yahoo.in>
2019-05-23 14:51:55 +0530

    Add type-mutation and type coercion exmple

--- JS-Basic/script.js
+++ JS-Basic/script.js
@@ -1,12 +1,28 @@
+// Simple print to console.
 console.log( 'Hello World!' );
 
 var name = 'Kishan';
 
+// String concat
 var lastName = 'Jasani'
 console.log( 'Hello ' + name + ' ' + lastName );
 
-var age = '23';
+// Number
+var age = 23;
 console.log( age );
 
+// Boolean
 var fullAge = true;
 console.log( fullAge );
+
+var job, isMarried;
+console.log( job ); // Undefined
+
+job =       'Web Developer';
+isMarried = true;
+console.log( name + ' is ' + age + ' ' + job + ' '+ isMarried );
+
+// We are mutating the variables.
+var age = 'Twenty';
+job = 'Software developer';
+console.log( name + ' is ' + age + ' ' + job + ' '+ isMarried );
