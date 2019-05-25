c1fb2457c45d884766eb7c9415366fa0cbf3cecd
Kishan Jasani <kishanjasani007@yahoo.in>
2019-05-25 10:18:58 +0530

    Add if else statement and operator example

--- JS-Basic/script.js
+++ JS-Basic/script.js
@@ -26,3 +26,39 @@ console.log( name + ' is ' + age + ' ' + job + ' '+ isMarried );
 var age = 'Twenty';
 job = 'Software developer';
 console.log( name + ' is ' + age + ' ' + job + ' '+ isMarried );
+
+// Operators
+var today = new Date();
+var birthYear = today.getFullYear() - 1996;
+console.log( birthYear );
+
+var ageKishan = 30;
+var ageManav = 30;
+
+ageKishan =  3 + 5 * 4 -6; // 17
+ageKishan = ageManav = ( 3 + 5 ) * 4 -6;
+// console.log( ageManav );
+// console.log( ageKishan );
+
+ageKishan++;
+ageManav *= 2;
+console.log( ageKishan );
+console.log( ageManav );
+
+// if/else statement
+
+var name = 'Kishan';
+var age = 26;
+var isMarried = 'yes';
+// isMarried = prompt('Is Married ? yes/no ');
+if ( isMarried === 'yes' ) {
+    console.log( name + ' is married' );
+} else {
+    console.log( name + ' is not married' );
+}
+
+if ( 23 === "23") {
+    console.log( 'Yes equal' );
+} else {
+    console.log( 'Not equal' );
+}
